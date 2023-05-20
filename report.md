d3d0a76cad2f904f2d2ee95b3ff12bde9bd782b8
artGr7 <gr-arturo@yandex.ru>
2023-05-20 12:13:39 +0300

    added string value

--- report.md
+++ report.md
@@ -4,4 +4,6 @@
 =======
 3. changed patch 1
 4. changed code style
+5. merged patch1
+6. added string value
 >>>>>>> patch1
--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+#include <string>
 
 int main() {
-    std::cout<<"Hello world";
+    string name;
+    std:: cin>>name;
+    std::cout<<"Hello world"<<name;
 }
